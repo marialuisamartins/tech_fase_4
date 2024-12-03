f6be8261e5f3fef2f7ff2dc8e9201e8f33d2520f
Maria Luisa Martins Brasil <70462627+marialuisamartins@users.noreply.github.com>
2024-12-03 20:46:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import pickle
 import pandas as pd
 from prophet import Prophet
 
-
-
 # Função para carregar o modelo Prophet
 @st.cache_resource
 def carregar_modelo():
@@ -22,6 +20,15 @@ def carregar_modelo():
     
     return modelo
 
+def dados_xls():
+    url = 'https://raw.githubusercontent.com/marialuisamartins/tech_fase4/6ad3e07bc901fd984eedb3030510b2816aaf7383/ipeadata%5B03-11-2024-01-09%5D.xlsx'
+    arquivo_local = 'ipeadata.xlsx'
+    response = requests.get(url)
+    with open(arquivo_local, 'wb') as file:
+        file.write(response.content)
+
+    return arquivo_local
+
 # Função principal do aplicativo
 def main():
     # Configuração da página
@@ -33,6 +40,53 @@ def main():
     # Carregar o modelo Prophet
     modelo = carregar_modelo()
     st.success("Modelo carregado com sucesso!")
+
+    dados_xls()
+
+    # Ler o arquivo Excel
+    ipeadata = pd.read_excel('ipeadata.xlsx', engine='openpyxl')
+
+    '''
+    # Definir a coluna 'data' como índice e filtrar para a partir de 2021
+ipeadata['data'] = pd.to_datetime(ipeadata['data'])
+ipeadata.set_index('data', inplace=True)
+ipeadata_filtered = ipeadata[ipeadata.index >= '2021-01-01']
+
+# Adicionar cabeçalho superior estilizado com HTML
+st.markdown("""
+    <style>
+        .title {
+            text-align: center;
+            font-size: 36px;
+            color: #2C3E50;
+            font-weight: bold;
+            margin-top: 20px;
+        }
+        .header-bar {
+            background-color: #2C3E50;
+            padding: 10px;
+            color: white;
+            text-align: center;
+            font-size: 24px;
+            font-weight: bold;
+        }
+    </style>
+    <div class="header-bar">Dashboard Interativo de Previsão e Análise do Preço do Petróleo</div>
+    <div class="title">Exploração de Dados e Previsões</div>
+""", unsafe_allow_html=True)
+
+# Barra lateral com logo da FIAP
+st.sidebar.image('https://upload.wikimedia.org/wikipedia/commons/5/54/Logo_FIAP.png', use_container_width=True)
+st.sidebar.header("Exploração do Preço do Petróleo")
+st.sidebar.write("Análise detalhada de preços e previsões para insights de mercado.")
+
+# Informações estatísticas importantes
+st.markdown("### Estatísticas e Insights Importantes")
+st.write(f"**Valor máximo no período**: ${ipeadata_filtered['preco'].max():,.2f}")
+st.write(f"**Valor mínimo no período**: ${ipeadata_filtered['preco'].min():,.2f}")
+st.write(f"**Média do período**: ${ipeadata_filtered['preco'].mean():,.2f}")
+st.write(f"**Desvio Padrão do período**: ${ipeadata_filtered['preco'].std():,.2f}")
+    '''
 
     # Entradas do usuário para previsão
     st.sidebar.header("Configurações da Previsão")
